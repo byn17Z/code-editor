8566142c5a00494bc59ebc6e07c6b44100b12762
byn17Z <147910899+byn17Z@users.noreply.github.com>
2024-11-19 18:07:35 +0800

    update v0.3
    实现文件管理功能：（File菜单）
    New
    Open
    Save
    Save as
    Close

--- editor_temp2/widget.cpp
+++ editor_temp2/widget.cpp
@@ -1,38 +1,66 @@
 #include "widget.h"
 #include "mytexteditor.h"
 #include <QPushButton>
-#include <QLine>
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
 #include <QTextEdit>
 #include <QTextBrowser>
 #include <QStatusBar>
-#include <QFrame>
+#include <QFileDialog>
+#include <QCoreApplication>
+#include <QIODevice>
+#include <QFile>
+#include <QMessageBox>
 
+
+//CONSTRUCTOR
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
+
     // set window
     this->setFixedSize(900, 600);
     this->setWindowTitle("Editor");
 
 
+
     // set tool bar
-    QPushButton* btnFile = new QPushButton("File", this);
-    btnFile->move(0, 0);
+    QMenuBar* myBar = new QMenuBar(this);
 
-    QPushButton* btnSave = new QPushButton("Save", this);
-    btnSave->move(75, 0);
+        QMenu* menuFile = new QMenu("File", this);
+        myBar->addMenu(menuFile);
 
-    QPushButton* btnEdit = new QPushButton("Edit", this);
-    btnEdit->move(150, 0);
+            QAction* actNew = new QAction("New", this);
+            menuFile->addAction(actNew);
+            connect(actNew, &QAction::triggered, this, &Widget::newSlot);
 
-    QPushButton* btnDebug = new QPushButton("Debug", this);
-    btnDebug->move(225, 0);
+            QAction* actOpen = new QAction("Open", this);
+            menuFile->addAction(actOpen);
+            connect(actOpen, &QAction::triggered, this, &Widget::openSlot);
 
-    QPushButton* btnCompile = new QPushButton("Compile", this);
-    btnCompile->move(300, 0);
+            QAction* actSave = new QAction("Save", this);
+            menuFile->addAction(actSave);
+            connect(actSave, &QAction::triggered, this, &Widget::saveSlot);
 
-    QPushButton* btnRun = new QPushButton("Run", this);
-    btnRun->move(375, 0);
+            QAction* actSaveAs = new QAction("Save As", this);
+            menuFile->addAction(actSaveAs);
+            connect(actSaveAs, &QAction::triggered, this, &Widget::saveAsSlot);
+
+            QAction* actClose = new QAction("Close", this);
+            menuFile->addAction(actClose);
+            connect(actClose, &QAction::triggered, this, &Widget::closeSlot);
+
+
+        QMenu* menuEdit = new QMenu("Edit", myBar);
+        myBar->addMenu(menuEdit);
+
+        QMenu* menuDebug = new QMenu("Debug", myBar);
+        myBar->addMenu(menuDebug);
+
+        QMenu* menuCompile = new QMenu("Compile", myBar);
+        myBar->addMenu(menuCompile);
+
 
     QPushButton* btnRefresh = new QPushButton("Refresh", this);
     btnRefresh->move(450, 0);
@@ -41,27 +69,23 @@ Widget::Widget(QWidget *parent)
 
     // set edit area
     MyTextEditor *textEditor = new MyTextEditor(this);
-
-
-    connect(btnFile, &QAbstractButton::pressed, textEditor, &QTextEdit::clear);
-    connect(btnSave, &QAbstractButton::pressed, textEditor, &QTextEdit::clear);
-    connect(btnEdit, &QAbstractButton::pressed, textEditor, &QTextEdit::clear);
-    connect(btnDebug, &QAbstractButton::pressed, textEditor, &QTextEdit::clear);
-    connect(btnCompile, &QAbstractButton::pressed, textEditor, &QTextEdit::clear);
-    connect(btnRun, &QAbstractButton::pressed, textEditor, &QTextEdit::clear);
-    connect(btnRefresh, &QAbstractButton::pressed, textEditor, &MyTextEditor::updateTextEditor);
-
+    this->setEditor(textEditor);
+    //TEST
     // std::string strHtml = "<span style=\"color: red;\">Edit Area</span>";
     // QString qstrHtml = QString::fromStdString(strHtml);
     // textEditor->QTextEdit::setHtml(qstrHtml);
+    // connect(textEditor, &QTextEdit::textChanged, this, &Widget::setisCFileSaved);
 
+
+
+    // connect(btnRefresh, &QAbstractButton::pressed, textEditor, &MyTextEditor::updateTextEditor);
     // connect(textEditor, &QTextEdit::textChanged, this, &Widget::setMyText);
 
 
 
 
     // set terminal bar
-    QTextEdit* term = new QTextEdit(this);
+    QTextEdit* term = new QTextBrowser(this);
     term->resize(800, 160);
     term->move(50, 420);
 
@@ -74,6 +98,262 @@ Widget::Widget(QWidget *parent)
     statusBar->showMessage("Ready", 1000);
 }
 
+
+
+
+//QPROPEERTY myEditor
+//READ - myEditor
+MyTextEditor* Widget::myEditor()
+{
+    return this->m_myEditor;
+}
+
+
+//WRITE - myEditor
+void Widget::setEditor(MyTextEditor* textEditor)
+{
+    this->m_myEditor = textEditor;
+}
+
+
+//SIGNAL - myEditor
+// void Widget::myEditorChanged() {}
+
+
+
+
+//QPROPEERTY cFilePath
+//READ - cFilePath
+QString Widget::cFilePath()
+{
+    return this->m_cFilePath;
+}
+
+
+//WRITE - cFilePath
+void Widget::setcFilePath(QString path)
+{
+    this->m_cFilePath = path;
+}
+
+
+//SIGNAL - cFilePath
+// void cFilePathChanged() {}
+
+
+// //QPROPEERTY isCFileSaved
+// //READ - isCFileSaved
+// bool Widget::isCFileSaved()
+// {
+//     return this->m_isCFileSaved;
+// }
+
+
+// //WRITE - isCFileSaved
+// void Widget::setisCFileSaved()
+// {
+//     qDebug() << this->m_isCFileSaved;
+//     this->m_isCFileSaved = 0;
+// }
+
+
+// //SIGNAL - isCFileSaved
+// // void Widget::isCFileSavedChanged() {}
+
+
+
+
+//ACTION SLOTS
+
+
+//actNew 新建文件
+void Widget::newSlot()
+{
+    // qDebug() << this->m_isCFileSaved;
+
+    // prompt用户选择新建路径
+    QString fileName = QFileDialog::getSaveFileName(
+        this,
+        "new",
+        QCoreApplication::applicationFilePath());
+
+    if (fileName.isEmpty()) {return;}
+
+    else {
+        qDebug() << fileName;
+
+        // 关闭当前文件
+        this->Widget::closeSlot();
+
+        // 储存新建文件的路径为当前路径，供保存时使用
+        this->setcFilePath(fileName);
+
+        QFile file(fileName);
+        file.open(QIODevice::WriteOnly);
+        file.close();
+    }
+
+    // this->m_isCFileSaved = 0;
+}
+
+
+//actOpen 打开文件
+void Widget::openSlot()
+{
+    // qDebug() << this->m_isCFileSaved;
+
+    // prompt用户选择文件
+    QString fileName = QFileDialog::getOpenFileName(
+        this,
+        "open",
+        QCoreApplication::applicationFilePath(),
+        "*.cpp");
+
+    if (fileName.isEmpty()) {return;}
+
+    else {
+        qDebug() << fileName;
+
+        // 关闭当前文件
+        this->Widget::closeSlot();
+
+        // 储存打开文件的路径为当前路径，供保存时使用
+        this->setcFilePath(fileName);
+
+        // 从当前路径载入文件内容
+        QFile file(fileName);
+        file.open(QIODevice::ReadOnly);
+        QByteArray openContent = file.readAll();
+        this->m_myEditor->setPlainText(openContent);
+        file.close();
+
+        // this->m_isCFileSaved = 0;
+    }
+}
+
+
+//actSave 保存文件
+void Widget::saveSlot()
+{
+    // qDebug() << this->m_isCFileSaved;
+
+    // 未打开或创建文件时，跳转save as
+    if (this->m_cFilePath.isEmpty()) {this->Widget::saveAsSlot();}
+
+    else {
+        qDebug() << this->m_cFilePath;
+
+        // 保存文件到当前路径
+        QFile file(this->m_cFilePath);
+        file.open(QIODevice::WriteOnly);
+        QByteArray saveContent = this->m_myEditor->toPlainText().QString::toUtf8();
+        file.write(saveContent);
+
+        // this->m_saveSuccess = 1;
+
+        // this->m_isCFileSaved = 1;
+    }
+}
+
+
+//actSaveAs 另存为
+void Widget::saveAsSlot()
+{
+    // qDebug() << this->m_isCFileSaved;
+
+    // prompt用户选择保存路径
+    QString fileName = QFileDialog::getSaveFileName(
+        this,
+        "save as...",
+        QCoreApplication::applicationFilePath());
+
+    if (fileName.isEmpty()) {
+        // 用户有主动关闭/取消另存为窗口、未选择路径的可能性
+        // 保存失败
+        // 在actClose关闭文件的过程中，保存成功或失败涉及actClose是否继续的判断
+        this->m_saveSuccess = 0;
+        return;
+    }
+
+    else {
+        qDebug() << fileName;
+
+        // 保存文件到选择的路径
+        QFile file(fileName);
+        file.open(QIODevice::WriteOnly);
+        QByteArray saveContent = this->m_myEditor->toPlainText().QString::toUtf8();
+        file.write(saveContent);
+
+        // 如果用户还没有打开或创建过文件，则储存选择另存为的路径作为当前路径
+        if (this->m_cFilePath.isEmpty()) {
+            this->setcFilePath(fileName);
+        }
+
+        // 保存成功
+        this->m_saveSuccess = 1;
+
+        // this->m_isCFileSaved = 1;
+    }
+}
+
+
+//actClose 关闭文件并清空窗口
+void Widget::closeSlot()
+{
+    // qDebug() << this->m_isCFileSaved;
+
+    // 未打开文件且内容为空时，直接关闭（跳过）
+    if (this->m_cFilePath.isEmpty()) {
+        if (this->m_myEditor->toPlainText().isEmpty()) {return;}
+    }
+
+    // 有打开文件 或 未打开文件但有内容 时，提示是否保存
+    QMessageBox* msgBox = new QMessageBox(this);
+        msgBox->setText("Warning");
+        msgBox->setInformativeText("Do you want to save your changes");
+        msgBox->setStandardButtons(QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+        msgBox->setDefaultButton(QMessageBox::Save);
+
+    int ret = msgBox->exec();
+
+    switch (ret) {
+    case QMessageBox::Save:
+        // Save was clicked
+        // 保存
+        this->Widget::saveSlot();
+
+        // 清空窗口（如果用户在跳转另存为后关闭了另存为窗口，未完成保存过程，则放弃清空）
+        if (this->m_saveSuccess) {
+            this->m_cFilePath = "";
+            this->m_myEditor->clear();
+        }
+
+        // this->m_isCFileSaved = 1;
+        break;
+
+    case QMessageBox::Discard:
+        // Don't Save was clicked
+        // 只清空不保存
+        this->m_cFilePath = "";
+        this->m_myEditor->clear();
+        // this->m_isCFileSaved = 1;
+        break;
+
+    case QMessageBox::Cancel:
+        // Cancel was clicked
+        break;
+
+    default:
+        // should never be reached
+        break;
+
+    }
+}
+
+
+
+
+//DECONSTRUCTOR
 Widget::~Widget() {}
 
 
--- editor_temp2/widget.h
+++ editor_temp2/widget.h
@@ -2,6 +2,7 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include "mytexteditor.h"
 
 class Widget : public QWidget
 {
@@ -10,6 +11,68 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+
+    //QPROPEERTY myEditor
+    Q_PROPERTY(MyTextEditor* myEditor MEMBER m_myEditor READ myEditor WRITE setEditor NOTIFY myEditorChanged)
+
+    //QPROPEERTY cFilePath
+    Q_PROPERTY(QString cFilePath MEMBER m_cFilePath READ cFilePath WRITE setcFilePath NOTIFY cFilePathChanged)
+
+    // //QPROPEERTY cFilePath
+    // Q_PROPERTY(QString isCFileSaved MEMBER m_isCFileSaved READ isCFileSaved WRITE setisCFileSaved NOTIFY isCFileSavedChanged)
+
+
+    //READ - myEditor
+    MyTextEditor* myEditor();
+
+    //READ - cFilePath
+    QString cFilePath();
+
+    // //READ - isCFileSaved
+    // bool isCFileSaved();
+
+
+public slots:
+
+    //WRITE - myEditor
+    void setEditor(MyTextEditor* textEditor);
+
+    //WRITE - cFilePath
+    void setcFilePath(QString path);
+
+    // //WRITE - isCFileSaved
+    // void setisCFileSaved();
+
+
+    //ACTION SLOTS
+    void newSlot();
+    void openSlot();
+    void saveSlot();
+    void saveAsSlot();
+    void closeSlot();
+
+
+signals:
+
+    //SIGNAL - myEditor
+    void myEditorChanged();
+
+    //SIGNAL - cFilePath
+    void cFilePathChanged();
+
+    // //SIGNAL - isCFileSaved
+    // void isCFileSavedChanged();
+
+private:
+
+    MyTextEditor* m_myEditor;
+
+    QString m_cFilePath = "";
+
+    bool m_saveSuccess = 0;
+
+    // bool m_isCFileSaved = 1;
+
 };
 
 #endif // WIDGET_H
