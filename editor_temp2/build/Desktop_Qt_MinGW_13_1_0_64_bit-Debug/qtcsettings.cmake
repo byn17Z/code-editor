c278978ef4262b1a7dd08fabe328852f5375e630
byn17Z <147910899+byn17Z@users.noreply.github.com>
2024-11-22 22:51:18 +0800

    adjust

--- editor_temp2/build/Desktop_Qt_MinGW_13_1_0_64_bit-Debug/qtcsettings.cmake
+++
@@ -1,3 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/Study/CSC/3002/Project/editor_temp2/build/Desktop_Qt_MinGW_13_1_0_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
